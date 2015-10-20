95479f2c2ce81a9aa0fdc0874e4ad569889a3b67
root <root@localhost.localdomain>
2015-10-20 13:21:24 +0200

    Modification for get HelloWorld

--- app/app.php
+++ app/app.php
@@ -1,17 +1,12 @@
 <?php
 
-require_once __DIR__.'/bootstrap.php';
-
-use Symfony\Component\HttpFoundation\Response;
-
-$app = new Silex\Application();
 
 $app->get('/', function() {
-	return new Response('Hello World');
+	return ('Hello World');
 });
 
 $app->get('/hello/{name}', function ($name) use ($app) {
-	return new Response('Hello ' . $app->escape($name)); 
+	return ('Hello ' . $app->escape($name)); 
 });
 
 
--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
 
-$app = require_once __DIR__.'/../app/app.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-$app->run(); 
+$app = new Silex\Application();
 
+require __DIR__.'/../app/app.php';
+
+
+$app->run();
 
 ?>
