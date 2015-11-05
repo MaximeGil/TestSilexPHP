92b2840f4f7c028c2988dbea9b15d8305d1acfaa
Maxime Gil <maxime.gil.pro@gmail.com>
2015-11-05 08:43:06 +0100

    Test for request API (json)

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -21,6 +21,16 @@ class HelloWorldTest extends WebTestCase
         $this->assertCount(1, $crawler->filter('h1:contains("Hello World")'));
     }
 
+    public function testAcceptJson()
+    {
+	$excepted = json_encode(array('first' => 'Hello', 'second' => 'World'));	
+	$client = $this->createClient();
+	$client->request('GET', '/api/hello', array(), array(), array('HTTP_ACCEPT' => 'application/json' , 'CONTENT_TYPE' => 'application/json')); 
+	$response = $client->getResponse();
+	$data = $response->getContent();
+	$this->assertEquals($excepted, $data);
+    }
+
     public function testHelloToto()
     {
         $client = $this->createClient();
