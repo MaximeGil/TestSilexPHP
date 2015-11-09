45764813449393725c870c1b322b59720664083b
Maxime Gil <maxime.gil.pro@gmail.com>
2015-11-09 14:48:34 +0100

    Replace with old app.php

--- app/app.php
+++ app/app.php
@@ -4,7 +4,6 @@ require_once __DIR__.'/bootstrap.php';
 
 use KPhoen\Provider\NegotiationServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
-use Negotiation\Stack\Negotiation;
 
 $app = new Silex\Application();
 $app->register(new NegotiationServiceProvider());
@@ -27,36 +26,29 @@ $app->get('/hello/{name}', function ($name) use ($app) {
 
 $app->get('/api/hello', function (Request $request) use ($app) {
 
-$app = new Negotiation($app);
-$app->handle($request);
-$format = $request->attributes->get('_format');
+    $negotiator = $app['format.negotiator'];
+    $acceptHeader = $request->headers->get('Accept');
+    $bestFormat = $negotiator->getBestFormat($acceptHeader, array('json', 'html'));
 
-var_dump($format);
-die(); 
+    switch ($bestFormat) {
+    case 'json':
+    return json_encode(array('first' => 'Hello', 'second' => 'World'));
+    break;
 
-switch($format) {
-case 'html':
-return $app['twig']->render('hello.twig.html', array('name' => 'World'));
-break;
+    case 'html':
+    return $app['twig']->render('hello.twig.html', array('name' => 'World'));
+    break;
 
-case 'json':
-return json_encode(array('first' => 'Hello', 'second' => 'World'));
-break;
-
-}
+    }
 
 });
 
 $app->get('/api/hello/', function () use ($app) {
- 
-return json_encode(array('first' => 'Hello', 'second' => 'You'));
+    return json_encode(array('first' => 'Hello', 'second' => 'You'));
 });
 
 $app->get('/api/hello/{name}', function ($name) use ($app) {
-    
- return json_encode(array('first' => 'Hello', 'second' => $name));
-
-
+    return json_encode(array('first' => 'Hello', 'second' => $name));
 });
 
 return $app;
